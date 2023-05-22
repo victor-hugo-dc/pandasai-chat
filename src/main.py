2682ceed495659f6d50a9769ca290e5bf70d251d
Victor Del Carpio <vhdelcg@gmail.com>
2023-05-22 01:02:14 -0400

    chore: move constants to separate file

--- src/main.py
+++ src/main.py
@@ -4,45 +4,7 @@ from streamlit_chat import message
 
 import pandas as pd
 from pandasai import PandasAI
-from pandasai.llm.openai import OpenAI
-from pandasai.llm.open_assistant import OpenAssistant
-from pandasai.llm.starcoder import Starcoder
-
-
-file_format = {
-    "csv": pd.read_csv,
-    "xls": pd.read_excel,
-    "xlsx": pd.read_excel,
-    "xlsm": pd.read_excel,
-    "xlsb": pd.read_excel,
-    "json": pd.read_json,
-    "html": pd.read_html,
-    "sql": pd.read_sql,
-    "feather": pd.read_feather,
-    "parquet": pd.read_parquet,
-    "dta": pd.read_stata,
-    "sas7bdat": pd.read_sas,
-    "h5": pd.read_hdf,
-    "hdf5": pd.read_hdf,
-    "pkl": pd.read_pickle,
-    "pickle": pd.read_pickle,
-    "gbq": pd.read_gbq,
-    "orc": pd.read_orc,
-    "xpt": pd.read_sas,
-    "sav": pd.read_spss,
-    "gz": pd.read_csv,
-    "zip": pd.read_csv,
-    "bz2": pd.read_csv,
-    "xz": pd.read_csv,
-    "txt": pd.read_csv,
-    "xml": pd.read_xml,
-}
-
-models = {
-    "OpenAI": OpenAI,
-    "Starcoder": Starcoder,
-    "Open-Assistant": OpenAssistant
-}
+from constants import file_format, models
 
 @st.cache_data
 def load_data(uploaded_file):
@@ -54,7 +16,7 @@ def load_data(uploaded_file):
         return None
 
 def generate_response(question_input, dataframe, option, api_key):
-    llm = models[option](api_token=api_key)
+    llm = models[option](api_key)
     pandas_ai = PandasAI(llm, conversational=False)
     return pandas_ai.run(dataframe, prompt=question_input, is_conversational_answer=True)
 
@@ -71,7 +33,7 @@ question_input = st.text_input("Enter question")
 
 left, right = st.columns([1, 2])
 with left:
-    model_option = st.selectbox('Model', ('OpenAI', 'Starcoder', 'Open-Assistant'))
+    model_option = st.selectbox('Model', models.keys())
 
 with right:
     api_key = st.text_input('API Key', '')
